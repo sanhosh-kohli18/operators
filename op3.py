c459993f9ef384eeaef7a98b818f2e4173012df9
SANTHOSH5A0 <santoshsantosh85118@gmail.com>
2024-10-01 11:13:58 +0530

    Create op3.py

---
+++ op3.py
@@ -0,0 +1,18 @@
+'''
+Extras are runs scored by a means other than a batsman hitting the ball. A batsman is not given credit for extras other than runs scored off the bat from a no ball, and the extras are tallied separately on the scorecard and count only towards the team’s score. the types of extras are No ball, Wide, Bye, Leg-bye and Penalty. 1 Penalty corresponds to 5 runs.
+Find the total runs that the Extras contribute to the team’s score, given the number of No-balls, wides, byes, leg-byes and penalty given off by the bowlers in innings.
+
+'''
+Sample input and output 1:
+4
+7
+3
+10
+3
+39
+Sample input and output 2:
+2
+3
+7
+1
+17
